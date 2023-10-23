e63f80245032e2ca8f8f24ae86c6435fd0ab4cec
MarianaSilvaC <79737436+MarianaSilvaC@users.noreply.github.com>
2023-10-22 22:26:17 -0700

    ej 6: table sales complete with api
    * ej 5: added lecture of json file to generate item cards
    
    tarjetas de videojuegos funcionales
    
    * ej 5: added lecture of json file to generate item cards
    
    * ej 5: auto complete and item search
    
    filtros y autocompletado
    
    * ej 5: functional filters
    
    * ej 5: functional filters medium
    
    * ej 6: fix card generation
    
    * ej 6: added rating filter
    
    * ej 6: Modular code and functional clear and search buttons
    
    * ej 6: Modular code
    
    * ej 6: functional autocomplete and generator star complete
    
    * ej 6: all catalogue functional with APIgames
    
    * ej 6: configuration covers
    
    * ej 6: API Catalogue functional and more games added
    
    * ej 6: modified search button and functional modal with api
    
    * ej 6: table of sales complete with api
    
    ---------
    
    Co-authored-by: Luis Romo <72539524+luisrp98@users.noreply.github.com>

--- src/sales/script.js
+++ src/sales/script.js
@@ -4,3 +4,16 @@ const btn_sell = document.querySelector('.btn-sell')
 btn_sell.addEventListener('click', () => {
     crearModalSell()
 })
+
+// Selecciona el botón "Cancelar" en el modal
+const btnCancel = document.getElementById('btn-cancelar');
+
+// Selecciona el modal
+const modal = document.querySelector('.modal-bg');
+
+// Agrega un controlador de eventos al botón "Cancelar"
+btnCancel.addEventListener('click', () => {
+    // Oculta el modal
+    modal.style.display = 'none';
+});
+
