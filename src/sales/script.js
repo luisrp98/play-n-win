b5c94336e4f87d97e9a5c06bdaac35c4530b3770
Luis Romo <72539524+luisrp98@users.noreply.github.com>
2023-10-23 10:03:15 -0700

    Revert "ej 6: sales 1/2"
    This reverts commit e6d2ddf573f8381dd73af534c8101c3d27d2d45c.

--- src/sales/script.js
+++ src/sales/script.js
@@ -5,5 +5,15 @@ btn_sell.addEventListener('click', () => {
     crearModalSell()
 })
 
+// Selecciona el botón "Cancelar" en el modal
+const btnCancel = document.getElementById('btn-cancelar');
 
+// Selecciona el modal
+const modal = document.querySelector('.modal-bg');
+
+// Agrega un controlador de eventos al botón "Cancelar"
+btnCancel.addEventListener('click', () => {
+    // Oculta el modal
+    modal.style.display = 'none';
+});
 
