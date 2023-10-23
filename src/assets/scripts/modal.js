542c5948051231f68bce5a5423b47cc24137e626
Luis Romo Portillo <luisrp9812@gmail.com>
2023-10-23 13:02:09 -0700

    ej 6: added functionality to the add item btn

--- src/assets/scripts/modal.js
+++ src/assets/scripts/modal.js
@@ -23,12 +23,11 @@ export function createModal(modaltype, game = null) {
     } else if (modaltype === 'modal-sale') {
         modalInnerHTML = `<div class="modal-sell border-r-1"><div class="modal-form f-display"><form action="" id="add-sale"><legend>Num. de orden</legend><p id="modal-num-order"></p><legend>Producto</legend><select name="producto" id="producto-select"><option disabled selected>Selecciona un producto</option><!-- Aquí se agregan las opciones desde la API --></select><input type="number" name="cantidad" id="cantidad-input" /><input type="button" value="Agregar" class="btn" id="btn-agregar" /><legend>Vendedor</legend><select name="vendedor" id="vendedor-select"><option disabled selected>Selecciona un vendedor</option><option value="">Mariana Silva</option><option value="">Luis Romo</option></select><legend>Fecha</legend><input type="date" /></form><div class="cart"><div class="cart-item"><table><thead><th>Título</th><th>Precio</th><th>Cantidad</th><th>Subtotal</th></thead><tbody id="cart-body"><!-- Aquí se agregarán las filas de productos --></tbody></table></div></div></div><div class="modal-form-btn"><input type="button" value="Agregar venta" id="btn-submit" class="btn btn-sell" /><input type="button" value="Cancelar" id="btn-clear" class="btn btn-delete" /></div></div>`
 
-        console.log('1')
         fetch('https://6534761be1b6f4c59046be6a.mockapi.io/api/games/videojuegos')
             .then((response) => response.json())
             .then((data) => {
                 const selectProducto = document.querySelector('#producto-select')
-                selectProducto.innerHTML = '' // Limpia las opciones existentes
+                // selectProducto.innerHTML = '' // Limpia las opciones existentes
                 data.forEach((producto) => {
                     const option = document.createElement('option')
                     option.value = producto.titulo
@@ -39,7 +38,6 @@ export function createModal(modaltype, game = null) {
             .catch((error) =>
                 console.error('Error al cargar la lista de productos: ', error)
             )
-        console.log('2')
     }
 
     // Agrega el elemento modal al modalbg
--- src/sales/script.js
+++ src/sales/script.js
@@ -1,6 +1,24 @@
 import { createModal } from '../assets/scripts/modal.js'
+import { addRowCart } from '../assets/scripts/añadirFila.js'
+
 const btn_sell = document.querySelector('.btn-sell')
+const btn_add = document.querySelector('#btn-agregar')
 
 btn_sell.addEventListener('click', () => {
     createModal('modal-sale')
+})
+// btn_add.addEventListener('DOMContentLoaded', function () {
+//     // Code here waits to run until the DOM is loaded.
+//     console.log('Aaaaaaa')
+// })
+// Agrega un oyente de eventos al elemento padre que ya existe (por ejemplo, el documento o algún contenedor existente).
+document.addEventListener('click', function (event) {
+    const target = event.target
+
+    // Verifica si el elemento que desencadenó el evento es el botón "boton-agregar".
+    if (target && target.id === 'btn-agregar') {
+        addRowCart()
+        // Aquí puedes realizar la lógica que deseas cuando se hace clic en el botón "boton-agregar".
+        // Por ejemplo, puedes abrir el modal de venta correspondiente.
+    }
 })
