de5ddc550b0996efa819122b4997a65a40daef25
MarianaSilvaC <marscamargo123@gmail.com>
2023-10-22 22:13:14 -0700

    ej 6: table of sales complete with api

--- src/sales/script.js
+++ src/sales/script.js
@@ -3,3 +3,16 @@ const btn_sell = document.querySelector('.btn-sell')
 btn_sell.addEventListener('click', () => {
     console.log('Sell')
 })
+
+// Selecciona el botón "Cancelar" en el modal
+const btnCancel = document.getElementById('btn-cancelar');
+
+// Selecciona el modal
+const modal = document.querySelector('.modal-bg');
+
+// Agrega un controlador de eventos al botón "Cancelar"
+btnCancel.addEventListener('click', () => {
+    // Oculta el modal
+    modal.style.display = 'none';
+});
+
