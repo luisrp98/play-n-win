34bc376e3367bdc7fcf40153a387324b96e8107d
Luis Romo Portillo <luisrp9812@gmail.com>
2023-10-23 19:33:31 -0700

    ej 6: added delete sales confirm

--- src/assets/scripts/eliminarVenta.js
+++ src/assets/scripts/eliminarVenta.js
@@ -3,54 +3,58 @@ import { generateTableSales } from './generarTablaVentas.js'
 export function deleteSale() {
     const checkboxesMarcados = document.querySelectorAll('input[type="checkbox"]:checked')
 
-    checkboxesMarcados.forEach((checkbox) => {
-        const row = checkbox.closest('tr')
-        const numDeOrden = row.cells[1].textContent
+    let text =
+        'Estás apunto de eliminar estos registros\nEsta acción no puede ser deshecha'
+    if (confirm(text) == true) {
+        checkboxesMarcados.forEach((checkbox) => {
+            const row = checkbox.closest('tr')
+            const numDeOrden = row.cells[1].textContent
 
-        console.log('Num. de Orden:', numDeOrden)
-        const url = new URL(
-            'https://6534761be1b6f4c59046be6a.mockapi.io/api/games/ventas'
-        )
-        url.searchParams.append('numero-de-orden', numDeOrden)
+            const url = new URL(
+                'https://6534761be1b6f4c59046be6a.mockapi.io/api/games/ventas'
+            )
+            url.searchParams.append('numero-de-orden', numDeOrden)
 
-        fetch(url, {
-            method: 'GET',
-            headers: { 'content-type': 'application/json' },
-        })
-            .then((res) => {
-                if (res.ok) {
-                    return res.json() // Convierte la respuesta JSON en un objeto JavaScript
-                } else {
-                    throw new Error('No se pudo obtener los datos') // Maneja el error si la respuesta no es "ok"
-                }
+            fetch(url, {
+                method: 'GET',
+                headers: { 'content-type': 'application/json' },
             })
-            .then((data) => {
-                // Aquí puedes trabajar con los datos obtenidos, por ejemplo, imprimirlos en la consola
-                console.log(data[0].id)
-                fetch(
-                    `https://6534761be1b6f4c59046be6a.mockapi.io/api/games/ventas/${data[0].id}`,
-                    {
-                        method: 'DELETE',
+                .then((res) => {
+                    if (res.ok) {
+                        return res.json() // Convierte la respuesta JSON en un objeto JavaScript
+                    } else {
+                        throw new Error('No se pudo obtener los datos') // Maneja el error si la respuesta no es "ok"
                     }
-                )
-                    .then((res) => {
-                        if (res.ok) {
-                            return res.json()
+                })
+                .then((data) => {
+                    // Aquí puedes trabajar con los datos obtenidos, por ejemplo, imprimirlos en la consola
+                    console.log(data[0].id)
+                    fetch(
+                        `https://6534761be1b6f4c59046be6a.mockapi.io/api/games/ventas/${data[0].id}`,
+                        {
+                            method: 'DELETE',
                         }
-                        // handle error
-                    })
-                    .then((task) => {
-                        // Do something with deleted task
-                    })
-                    .catch((error) => {
-                        // handle error
-                    })
-            })
-            .catch((error) => {
-                // Maneja errores, por ejemplo, mostrando un mensaje de error en la consola
-                console.error(error)
-            })
-    })
+                    )
+                        .then((res) => {
+                            if (res.ok) {
+                                return res.json()
+                            }
+                            // handle error
+                        })
+                        .then((task) => {
+                            // Do something with deleted task
+                        })
+                        .catch((error) => {
+                            // handle error
+                        })
+                })
+                .catch((error) => {
+                    // Maneja errores, por ejemplo, mostrando un mensaje de error en la consola
+                    console.error(error)
+                })
+        })
+    } else {
+    }
 
     const ventasTableBody = document.querySelector('#sales-results tbody')
 
