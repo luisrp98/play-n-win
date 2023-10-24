4a685581227b7cde76f1b40b110eba18c01822b6
Luis Romo Portillo <luisrp9812@gmail.com>
2023-10-23 19:25:44 -0700

    ej 6: added delete sales function

---
+++ src/assets/scripts/eliminarVenta.js
@@ -0,0 +1,63 @@
+import { generateTableSales } from './generarTablaVentas.js'
+
+export function deleteSale() {
+    const checkboxesMarcados = document.querySelectorAll('input[type="checkbox"]:checked')
+
+    checkboxesMarcados.forEach((checkbox) => {
+        const row = checkbox.closest('tr')
+        const numDeOrden = row.cells[1].textContent
+
+        console.log('Num. de Orden:', numDeOrden)
+        const url = new URL(
+            'https://6534761be1b6f4c59046be6a.mockapi.io/api/games/ventas'
+        )
+        url.searchParams.append('numero-de-orden', numDeOrden)
+
+        fetch(url, {
+            method: 'GET',
+            headers: { 'content-type': 'application/json' },
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json() // Convierte la respuesta JSON en un objeto JavaScript
+                } else {
+                    throw new Error('No se pudo obtener los datos') // Maneja el error si la respuesta no es "ok"
+                }
+            })
+            .then((data) => {
+                // Aquí puedes trabajar con los datos obtenidos, por ejemplo, imprimirlos en la consola
+                console.log(data[0].id)
+                fetch(
+                    `https://6534761be1b6f4c59046be6a.mockapi.io/api/games/ventas/${data[0].id}`,
+                    {
+                        method: 'DELETE',
+                    }
+                )
+                    .then((res) => {
+                        if (res.ok) {
+                            return res.json()
+                        }
+                        // handle error
+                    })
+                    .then((task) => {
+                        // Do something with deleted task
+                    })
+                    .catch((error) => {
+                        // handle error
+                    })
+            })
+            .catch((error) => {
+                // Maneja errores, por ejemplo, mostrando un mensaje de error en la consola
+                console.error(error)
+            })
+    })
+
+    const ventasTableBody = document.querySelector('#sales-results tbody')
+
+    while (ventasTableBody.firstChild) {
+        ventasTableBody.removeChild(ventasTableBody.firstChild)
+    }
+    setTimeout(() => {
+        generateTableSales()
+    }, 1000)
+}
--- src/sales/script.js
+++ src/sales/script.js
@@ -1,20 +1,24 @@
 import { createModal } from '../assets/scripts/modal.js'
 import { addRowCart } from '../assets/scripts/añadirFila.js'
 import { addSale } from '../assets/scripts/agregarVenta.js'
+import { deleteSale } from '../assets/scripts/eliminarVenta.js'
 import { generateTableSales } from '../assets/scripts/generarTablaVentas.js'
 
 generateTableSales()
 const btn_sell = document.querySelector('.btn-sell')
+const btn_delete = document.querySelector('.btn-delete')
 
 btn_sell.addEventListener('click', () => {
     createModal('modal-sale')
+})
+btn_delete.addEventListener('click', () => {
+    deleteSale()
 })
 
 // Agrega un oyente de eventos al elemento padre que ya existe (por ejemplo, el documento o algún contenedor existente).
 document.addEventListener('click', function (event) {
     const target = event.target
     const modalbg = document.querySelector('.modal-bg')
-    const cant = document.querySelector('#cantidad-input')
 
     // Verifica si el elemento que desencadenó el evento es el botón "boton-agregar".
     if (target && target.id === 'btn-agregar') {
